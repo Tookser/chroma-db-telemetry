5a35e6d2cf63cd9f7096aacb6e227477f5520c05
Tookser <t2468ttt@gmail.com>
2025-11-26 01:37:20 +0300

    version 0.5.2312, fix

--- chromadb/__init__.py
+++ chromadb/__init__.py
@@ -45,7 +45,7 @@ logger = logging.getLogger(__name__)
 
 __settings = Settings()
 
-__version__ = "0.5.2310"
+__version__ = "0.5.2312"
 
 
 # Workaround to deal with Colab's old sqlite3 version
--- deployments/gcp/main.tf
+++ deployments/gcp/main.tf
@@ -23,7 +23,7 @@ variable "machine_type" {
 
 variable "chroma_version" {
   description = "Chroma version to install"
-  default     = "0.5.2310"
+  default     = "0.5.2312"
 }
 
 variable "chroma_server_auth_credentials" {
