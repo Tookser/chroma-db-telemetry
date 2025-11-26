a629e82a9f5fa904c684fe6ba4d5bce404052977
Tookser <t2468ttt@gmail.com>
2025-11-26 03:47:26 +0300

    fix

--- deployments/gcp/main.tf
+++ deployments/gcp/main.tf
@@ -23,7 +23,7 @@ variable "machine_type" {
 
 variable "chroma_version" {
   description = "Chroma version to install"
-  default     = "0.5.2319"
+  default     = "0.5.2320"
 }
 
 variable "chroma_server_auth_credentials" {
